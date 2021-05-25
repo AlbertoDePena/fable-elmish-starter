4f217d4c2bc7fac1fd6a9ff6ea37e9e8dc98ba97
Alberto De Pena <alberto.depena@craneww.com>
2021-05-25 13:32:54 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: [
+        './src/**/*.html',
+        './src/**/*.fs.js'
+    ],
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: []
+}
