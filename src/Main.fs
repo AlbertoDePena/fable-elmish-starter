540efcd6ec4c5f9ef40cb2296130f514d75e5140
Alberto De Pena <albertodepena89@gmail.com>
2021-08-07 22:17:02 -0500

    Added .editorconfig

--- src/Main.fs
+++ src/Main.fs
@@ -4,43 +4,38 @@ open Elmish
 open Elmish.React
 open Feliz
 
-type State =
-    { Count: int }
+type State = { Count: int }
 
 type Msg =
     | Increment
     | Decrement
 
-let init() =
-    { Count = 0 }, Cmd.none
+let init () = { Count = 0 }, Cmd.none
 
 let update (msg: Msg) (state: State) =
     match msg with
-    | Increment ->
-        { state with Count = state.Count + 1 }, Cmd.none
+    | Increment -> { state with Count = state.Count + 1 }, Cmd.none
 
-    | Decrement ->
-        { state with Count = state.Count - 1 }, Cmd.none
+    | Decrement -> { state with Count = state.Count - 1 }, Cmd.none
 
 let render (state: State) (dispatch: Msg -> unit) =
-  Html.div [
-    prop.classes [ "container"; "mx-auto"; "py-4"; "px-4" ]
-    prop.children [
-      Html.button [
-        prop.classes [ "border"; "border-blue-600"; "text-gray-200"; "rounded"; "px-5"; "py-5"; "mx-2"; "bg-blue-500" ]
-        prop.onClick (fun _ -> dispatch Increment)
-        prop.text "Increment"
-      ]
+    let buttonStyle =
+        "border border-blue-600 text-gray-200 rounded px-5 py-5 mx-2 bg-blue-500"
 
-      Html.button [
-        prop.classes [ "border"; "border-blue-600"; "text-gray-200"; "rounded"; "px-5"; "py-5"; "mx-2"; "bg-blue-500" ]
-        prop.onClick (fun _ -> dispatch Decrement)
-        prop.text "Decrement"
-      ]
+    Html.div
+        [ prop.className "container mx-auto py-4 px-4"
+          prop.children
+              [ Html.button
+                  [ prop.className buttonStyle
+                    prop.onClick (fun _ -> dispatch Increment)
+                    prop.text "Increment" ]
 
-      Html.h1 state.Count
-    ]    
-  ]
+                Html.button
+                    [ prop.className buttonStyle
+                      prop.onClick (fun _ -> dispatch Decrement)
+                      prop.text "Decrement" ]
+
+                Html.h1 state.Count ] ]
 
 Program.mkProgram init update render
 |> Program.withReactSynchronous "elmish-app"
