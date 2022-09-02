3ec0dba6e142e508f59b96d5e0ee3de17f7872d2
Alberto De Pena <alberto.depena@craneww.com>
2022-09-02 08:42:24 -0500

    Split main from app

---
+++ Application.fs
@@ -0,0 +1,122 @@
+[<RequireQualifiedAccess>]
+module Application
+
+open Elmish
+open Elmish.React
+
+open Feliz
+
+type State =
+    { Count: int
+      RandomDeferred: Deferred<double> }
+
+type Msg =
+    | Increment
+    | Decrement
+    | GenerateRandomNumberAsync of AsyncMsg<double>
+
+let random = System.Random()
+
+let init () =
+    { Count = 0
+      RandomDeferred = Deferred.HasNotStartedYet },
+    Cmd.none
+
+let update (msg: Msg) (state: State) =
+    match msg with
+    | Increment -> { state with Count = state.Count + 1 }, Cmd.none
+
+    | Decrement -> { state with Count = state.Count - 1 }, Cmd.none
+
+    | GenerateRandomNumberAsync AsyncMsg.Started ->
+        let generateRandom () =
+            async {
+                do! Async.Sleep 1000
+                let randomNumber = random.NextDouble()
+                return randomNumber
+            }
+
+        let command =
+            Cmd.OfAsyncImmediate.perform generateRandom () (AsyncMsg.Completed >> GenerateRandomNumberAsync)
+
+        { state with RandomDeferred = Deferred.InProgress }, command
+
+    | GenerateRandomNumberAsync (AsyncMsg.Completed randomNumber) ->
+        { state with RandomDeferred = Deferred.Resolved randomNumber }, Cmd.none
+
+let render (state: State) (dispatch: Msg -> unit) =
+    Html.div [
+        prop.classes [
+            "container"
+            "mx-auto"
+            "my-6"
+            "box"
+        ]
+        prop.children [
+            Html.div [
+                prop.classes [ "block" ]
+                prop.children [
+                    Html.button [
+                        prop.classes [
+                            "button"
+                            "is-small"
+                            "m-1"
+                        ]
+                        prop.type'.button
+                        prop.onClick (fun _ -> dispatch Increment)
+                        prop.text "Increment"
+                    ]
+
+                    Html.button [
+                        prop.classes [
+                            "button"
+                            "is-small"
+                            "m-1"
+                        ]
+                        prop.type'.button
+                        prop.onClick (fun _ -> dispatch Decrement)
+                        prop.text "Decrement"
+                    ]
+
+                    Html.button [
+                        prop.classes [
+                            "button"
+                            "is-small"
+                            "m-1"
+                            if state.RandomDeferred = Deferred.InProgress then
+                                "is-loading"
+                        ]
+                        prop.type'.button
+                        prop.onClick (fun _ -> dispatch (GenerateRandomNumberAsync AsyncMsg.Started))
+                        prop.text "Random Number"
+                    ]
+                ]
+            ]
+
+            Html.div [
+                prop.classes [ "block" ]
+                prop.children [ Html.h1 state.Count ]
+            ]
+
+            Html.div [
+                prop.classes [ "block" ]
+                prop.children [
+                    match state.RandomDeferred with
+                    | Deferred.HasNotStartedYet -> Html.none
+                    | Deferred.InProgress -> Html.h1 "Please wait..."
+                    | Deferred.Resolved randomNumber -> Html.h1 randomNumber
+                ]
+            ]
+
+            Html.div [
+                prop.classes [ "block" ]
+                prop.children [
+                    Html.input [
+                        prop.classes [ "input"; "is-small" ]
+                        prop.type'.text
+                        prop.placeholder "Just an input..."
+                    ]
+                ]
+            ]
+        ]
+    ]
