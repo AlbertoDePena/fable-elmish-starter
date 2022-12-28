5f59131792e5061ffccdde7c2e1a3143fe25c2ff
Alberto De Pena <alberto.depena@craneww.com>
2022-12-28 08:14:50 -0600

    Use latest Elmish.Browser library

--- Main.fs
+++ Main.fs
@@ -7,15 +7,13 @@ open Elmish
 open Elmish.React
 open Elmish.Navigation
 
-open ADP.Fable
-
 importAll "bulma/css/bulma.min.css"
 
 Program.mkProgram Application.init Application.update Application.render
 |> Program.toNavigable (UrlParser.parseHash Application.parseUrl) Application.updateUrl
 |> Program.withReactSynchronous "elmish-app"
 #if DEBUG
-//|> Program.withTrace Tracers.console
+|> Program.withTrace Tracers.console
 |> Program.withConsoleTrace
 #endif
 |> Program.run
